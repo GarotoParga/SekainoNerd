a10ac614f761224d261d45e70be963f1b78bdf9e
ANDRE PARGA <andreparga@MacBook-Pro-de-ANDRE.local>
2025-02-22 01:39:01 -0300

    ajustes finais

--- imagem.js
+++ imagem.js
@@ -9,13 +9,21 @@ const minhaImagem = document.getElementById('minha-imagem');
 let indiceImagem = 0;
 
 function mudarImagem() {
+    // Suaviza a saída
+    minhaImagem.style.transition = 'opacity 1s ease-in-out';
     minhaImagem.style.opacity = 0;
 
     setTimeout(() => {
         indiceImagem = (indiceImagem + 1) % imagens.length;
         minhaImagem.src = imagens[indiceImagem];
-        minhaImagem.style.opacity = 1;
-    }, 1000); 
+
+        // Garante que a imagem carregou antes de exibir
+        minhaImagem.onload = () => {
+            minhaImagem.style.opacity = 1;
+        };
+    }, 1000);
 }
 
+// Inicia a troca de imagens
 setInterval(mudarImagem, 5000);
+
